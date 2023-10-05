8a0ee8264c2d22fbb73237e7dc9150611ae4ddf8
hossein439 <mozafarihossein439@gmail.com>
2023-10-05 10:40:28 +0330

    clean nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             baseUrl: process.env.BASE_URL,
         },
     },
-    // axios: {
-    //     // Do away with the baseUrl when using proxy
-    //     proxy: true
-    // },
-    // proxy: {
-    //     // Simple proxy
-    //     "/api/": {
-    //         target: "http://localhost:4000",
-    //         pathRewrite: { "^/api/": "" }
-    //     }
-    // },
     css: ['~/assets/css/custom.css'],
     modules: [
         '@nuxtjs/tailwindcss',
