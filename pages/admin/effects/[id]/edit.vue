18689844d1a455a2969311760ab59d2ce9b8c3fa
hossein439 <mozafarihossein439@gmail.com>
2023-10-09 17:58:12 +0330

    ui improvment

--- pages/admin/effects/[id]/edit.vue
+++ pages/admin/effects/[id]/edit.vue
@@ -69,7 +69,9 @@ const getSingle = async () => {
         method: 'get',
         url: `http://localhost:4000/effect/${route.params.id}`,
     });
-    const { link, image, alt, gif, name, category_id } = data.data[0];
+    const { name, link, image, alt, gif, categoryName, category_id } = data.data[0];
+
+    effect.name = name;
     effect.link = link;
     effect.image = image;
     effect.gif = gif;
@@ -88,6 +90,7 @@ getSingle()
 
 const edit = () => {
     const formData = new FormData();
+    formData.append('name', effect.name);
     formData.append('link', effect.link);
     formData.append('alt', effect.alt)
     formData.append('categoryId', effect.categoryId);
@@ -110,7 +113,16 @@ const edit = () => {
 
 <template>
     <form @submit.prevent="edit()">
-        <div class="w-1/2 flex flex-col gap-2">
+        <div class="grid grid-cols-2 gap-5">
+            <div>
+                <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div>
+                    <input v-model="effect.name" type="text" name="name" id="name"
+                        class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                        placeholder="">
+                </div>
+            </div>
+
             <div>
                 <label for="link" class="block text-sm font-medium leading-6 text-gray-900">Link</label>
                 <div>
@@ -168,32 +180,32 @@ const edit = () => {
                 </div>
             </div>
 
-            <div class="flex gap-2 mt-2">
-                <div :class="!imgSrc ? 'p-12' : 'p-3'"
-                    class="relative block w-full h-48 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <svg v-show="!imgSrc" class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none"
-                        viewBox="0 0 48 48" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
-                    </svg>
-                    <input @change="selectImage($event, 'img')" class="absolute inset-0 opacity-0 cursor-pointer"
-                        type="file">
-                    <span v-show="!imgSrc" class="mt-2 block text-sm font-semibold text-gray-900">Image for effect</span>
-                    <img v-show="imgSrc" :src="imgSrc" alt="" class="w-full h-full rounded-lg inline-block object-cover">
-                </div>
+            <div :class="!imgSrc ? 'p-12' : 'p-3'"
+                class="relative block w-full h-48 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <svg v-show="!imgSrc" class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none"
+                    viewBox="0 0 48 48" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
+                </svg>
+                <input @change="selectImage($event, 'img')" class="absolute inset-0 opacity-0 cursor-pointer"
+                    type="file">
+                <span v-show="!imgSrc" class="mt-2 block text-sm font-semibold text-gray-900">Image for effect</span>
+                <img v-show="imgSrc" :src="imgSrc" alt="" class="w-full h-full rounded-lg inline-block object-cover">
+            </div>
 
-                <div :class="!gifSrc ? 'p-12' : 'p-3'"
-                    class="relative block w-full h-48 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <svg v-show="!gifSrc" class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none"
-                        viewBox="0 0 48 48" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
-                    </svg>
-                    <input @change="selectImage($event, 'gif')" class="absolute inset-0 opacity-0 cursor-pointer"
-                        type="file">
-                    <span v-show="!gifSrc" class="mt-2 block text-sm font-semibold text-gray-900">Gif for effect</span>
-                    <img v-show="gifSrc" :src="gifSrc" alt="" class="w-full h-full rounded-lg inline-block object-cover">
-                </div>
+            <div :class="!gifSrc ? 'p-12' : 'p-3'"
+                class="relative block w-full h-48 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <svg v-show="!gifSrc" class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none"
+                    viewBox="0 0 48 48" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6" />
+                </svg>
+                <input @change="selectImage($event, 'gif')" class="absolute inset-0 opacity-0 cursor-pointer"
+                    type="file">
+                <span v-show="!gifSrc" class="mt-2 block text-sm font-semibold text-gray-900">Gif for effect</span>
+                <img v-show="gifSrc" :src="gifSrc" alt="" class="w-full h-full rounded-lg inline-block object-cover">
+            </div>
+            <div class="flex gap-2 mt-2">
             </div>
         </div>
 
