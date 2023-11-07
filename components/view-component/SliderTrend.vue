aa8327d8196a54e65bc48ccee0c044769d6c80d7
hossein439 <mozafarihossein439@gmail.com>
2023-11-07 22:14:13 +0330

    ui improvement

--- components/view-component/SliderTrend.vue
+++ components/view-component/SliderTrend.vue
@@ -47,11 +47,11 @@ const options = reactive({
 
 </script>
 <template>
-    <div class="mx-12 mt-12">
+    <div class="xs:mx-8 lg:mx-12 mt-12">
         <Splide :options="options" :has-track="false">
             <SplideTrack>
                 <SplideSlide v-for="filter in filters" :key="filter.id">
-                    <div class="slide-container h-[465px] relative z-10">
+                    <div class="slide-container xs:h-[130px] lg:h-[465px] relative z-10">
                         <img class="h-full object-cover rounded-2xl" :src="`_nuxt/uploads/${filter.gif}`" alt="">
                         <div class="slide-item absolute right-0 left-0 rounded-2xl flex flex-col text-center">
                             <div class="w-[30px] absolute z-50 top-4 left-4">
@@ -85,13 +85,13 @@ const options = reactive({
             </SplideTrack>
     
             <div class="splide__arrows relative -z-10 bottom-[22rem] flex justify-between mx-auto">
-                <button class="splide__arrow--prev mb-10 -ml-28 cursor-pointer bg-[#5593FF] text-[#E6EFFF] lg:w-[80px] xs:w-[33px] lg:h-[80px] xs:h-[33px] rounded-full relative shadow-md hover:bg-[#025EFF] transition-all">
+                <button class="splide__arrow--prev mb-10 xs:-ml-12 lg:-ml-28 cursor-pointer bg-[#5593FF] text-[#E6EFFF] lg:w-[80px] xs:w-[33px] lg:h-[80px] xs:h-[33px] rounded-full relative shadow-md hover:bg-[#025EFF] transition-all">
                     <svg class="absolute inset-0 w-full m-auto" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                         <path d="M12 24L20 16L12 8" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round" />
                     </svg>
                 </button>
-                <button class="splide__arrow--next mb-10 -mr-28 cursor-pointer bg-[#5593FF] text-[#E6EFFF] lg:w-[80px] xs:w-[33px] lg:h-[80px] xs:h-[33px] rounded-full relative shadow-md hover:bg-[#025EFF] transition-all">
+                <button class="splide__arrow--next mb-10 xs:-mr-12 lg:-mr-28 cursor-pointer bg-[#5593FF] text-[#E6EFFF] lg:w-[80px] xs:w-[33px] lg:h-[80px] xs:h-[33px] rounded-full relative shadow-md hover:bg-[#025EFF] transition-all">
                     <svg class="absolute inset-0 w-full m-auto rotate-270" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                         <path d="M12 24L20 16L12 8" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                             stroke-linejoin="round" />
