d764384f83e6f7aa91999e23c166c1715739457d
hossein439 <mozafarihossein439@gmail.com>
2023-12-22 23:21:47 +0330

    fix: fix bug

--- server/api/settings/[slug].js
+++ server/api/settings/[slug].js
@@ -7,15 +7,16 @@ export default defineEventHandler(async (event) => {
 
 
     let { data: getById, error } = await supabase
-        .from(process.env.TABLE_NAME_SETTING)
+        .from('settings')
         .select('*')
         .eq('meta', meta)
 
+    const table = process.env.TABLE_NAME_SETTING;
     if (error) {
         return {
             error,
             meta,
-            table: process.env.TABLE_NAME_SETTING
+            table
         }
     } else {
 
