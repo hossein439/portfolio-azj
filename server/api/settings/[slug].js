e99beeaaba1c1632112e30a3ecb3c26751401013
hossein439 <mozafarihossein439@gmail.com>
2023-12-22 23:19:23 +0330

    fix: fix bug

--- server/api/settings/[slug].js
+++ server/api/settings/[slug].js
@@ -4,13 +4,19 @@ export default defineEventHandler(async (event) => {
 
     const meta = getRouterParam(event, 'slug')
 
+
+
     let { data: getById, error } = await supabase
         .from(process.env.TABLE_NAME_SETTING)
         .select('*')
         .eq('meta', meta)
 
     if (error) {
-        return error
+        return {
+            error,
+            meta,
+            table: process.env.TABLE_NAME_SETTING
+        }
     } else {
 
         return getById
